a45f5f4c2b5569a43d696277232a44824a439e6f
TigranA <84371464+Tigran1991@users.noreply.github.com>
2022-06-13 01:29:25 +0400

    project performance improvement V1

--- src/Playfield.js
+++ src/Playfield.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import {React, memo} from 'react';
 
 import './App.css';
 import CharacterCell from './CharacterCell';
 import { FREE_CELL } from './RabbitWolfGameClass';
 
-const Playfield = (props) => {
+const Playfield = memo((props) => {
 
     return (
       <div className='playfield'>
 
-        {props.matrix.map((row, X) => 
-          row.map((rowItem, Y) =>
-            rowItem !== 0 ?
-            <CharacterCell item={rowItem} key={X + "" + Y}/> :
-            <CharacterCell item={FREE_CELL} key={X + "" + Y}/>
-          )    
-        )}
+        {
+          props.matrix.map((row, X) => 
+            row.map((rowItem, Y) =>
+              rowItem !== 0 ?
+              <CharacterCell item={rowItem} key={X + "" + Y}/> :
+              <CharacterCell item={FREE_CELL} key={X + "" + Y}/>
+            )    
+          )
+        }
         
       </div>
     )
-}
+})
 
 export default Playfield;
--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,19 +1,20 @@
-import { combineReducers, createStore } from 'redux';
-import { gameFieldReducer, initialGameField } from '../features/gameReducerSlice';
-import { sizeReducer, initialSize } from '../features/sizeReducerSlice';
-import { boardsReducer } from '../features/boardsReducerSlice';
-import { boardReducer } from '../features/boardReducerSlice';
+import { combineReducers, createStore } from "redux";
+import { gameFieldReducer, initialGameField } from "../features/gameReducerSlice";
+import { sizeReducer, initialSize } from "../features/sizeReducerSlice";
+import { boardsReducer } from "../features/boardsReducerSlice";
+import { boardReducer } from "../features/boardReducerSlice";
 
-const store = createStore(combineReducers({
-  gameField: gameFieldReducer,
-  currentSize: sizeReducer,
-  board: boardReducer,
-  boards: boardsReducer,
-}), {
-  gameField: initialGameField,
-  currentSize: initialSize,
-});
+const store = createStore(
+  combineReducers({
+    gameField: gameFieldReducer,
+    currentSize: sizeReducer,
+    board: boardReducer,
+    boards: boardsReducer,
+  }),
+  {
+    gameField: initialGameField,
+    currentSize: initialSize,
+  }
+);
 
 export default store;
-
-
